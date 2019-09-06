0e19e7561c7610301d98abd15739cf3231bc408b
Dongyang <leeslemon@yahoo.com>
2019-09-06 02:53:37 -0400

    ss
    sss

---
+++ hw3/js/unlock.js
@@ -0,0 +1,18 @@
+$(function() {
+  $("h2 img").draggable({
+    axis: 'x',
+    containment: 'parent',
+    drag: function(event, ui) {
+      if (ui.position.left > 550) {
+        $("#well").fadeOut();
+      }
+    },
+    stop: function(event, ui) {
+      if (ui.position.left < 551) {
+        $(this).animate({
+          left: 0
+        })
+      }
+    }
+  });
+});
